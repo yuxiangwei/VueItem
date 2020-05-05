2d931459a4b4e6c2d3e2bfbd1c8ae1098a544566
余祥伟 <347502918@qq.com>
2020-05-05 16:06:20 +0800

    创建手册提交基础

--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import fastClick from 'fastclick' //安装npm install fastclick --save 解决移动端点击延迟300ms问题
+import './assets/styles/reset.css' //重置移动端样式
+import './assets/styles/border.css' //解决移动端1px边框问题
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
+
 
 new Vue({
   router,
---
+++ vue.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  //基本路径
+  publicPath: './',//相对路径
+  assetsDir: 'static', //生成静态文件存放位置
+  productionSourceMap: false, // 设置上线后是否加载webpack文件
+}
